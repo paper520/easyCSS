ef9460d94081ba2863fe862a8619e19dc41b5402
心叶 <yelloxing@gmail.com>
2018-04-27 14:36:50 +0800

    恢复项目【原项目名称Bufferfly.css】

---
+++ merge.js
@@ -0,0 +1,34 @@
+/**
+ * 用来负责合并代码的脚本
+ */
+
+const fs = require("fs");
+const path = require('path');
+
+console.log('\x1B[37m%s\x1B[39m', new Date() + ':代码合并开始\n');
+
+// [读取核心代码]
+let content = fs.readFileSync(path.join('./src', 'core.css'), 'utf-8');
+fs.writeFile('./easycss-1.0.0.css', "/*!\n* [" + new Date() + "]\n*/\n" + content, { encoding: 'utf-8' }, function () {
+    console.log('\x1B[37m%s\x1B[39m', new Date() + ':核心代码合并成功\n');
+
+    //[读取完成特定功能的模块]
+    let files = [
+
+
+
+    ];
+
+    // 迭代目录下的每一个文件
+    files.forEach(function (filename) {
+        var filedir = path.join('./src', filename);
+        // 读取文件内容
+        let content = fs.readFileSync(path.join('./src', filename), 'utf-8');
+        // 读取的内容拼接到最终的文件中
+        fs.appendFile('./easycss-1.0.0.css', "\n/*!======= " + filename + " =======*/\n" + content, function () {
+            console.log('\x1B[37m%s\x1B[39m', new Date() + ':' + filename + '合并成功\n');
+        });
+    });
+
+});
+

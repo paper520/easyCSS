b957336ef66b884c064c1860438b597785c7b584
心叶 <yelloxing@gmail.com>
2018-07-18 09:42:39 +0800

    [7月18日]升级版本：去掉十二栅格化预定义样式

--- merge.js
+++ merge.js
@@ -9,7 +9,7 @@ console.log('\x1B[37m%s\x1B[39m', new Date() + ':代码合并开始\n');
 
 // [读取核心代码]
 let content = fs.readFileSync(path.join('./src', 'core.css'), 'utf-8');
-fs.writeFile('./easycss-1.0.0.css', "/*!\n* [" + new Date() + "]\n*/\n" + content, { encoding: 'utf-8' }, function () {
+fs.writeFile('./easycss-1.2.0.css', "/*!\n* [" + new Date() + "]\n*/\n" + content, { encoding: 'utf-8' }, function () {
     console.log('\x1B[37m%s\x1B[39m', new Date() + ':核心代码合并成功\n');
 
     //[读取完成特定功能的模块]
@@ -19,10 +19,7 @@ fs.writeFile('./easycss-1.0.0.css', "/*!\n* [" + new Date() + "]\n*/\n" + conten
         'normalize.css',
 
         // 浏览器样式重置
-        'reset.css',
-
-        // 十二栅格化
-        'rasterize.css'
+        'reset.css'
 
     ];
 
@@ -32,7 +29,7 @@ fs.writeFile('./easycss-1.0.0.css', "/*!\n* [" + new Date() + "]\n*/\n" + conten
         // 读取文件内容
         let content = fs.readFileSync(path.join('./src', filename), 'utf-8');
         // 读取的内容拼接到最终的文件中
-        fs.appendFile('./easycss-1.0.0.css', "\n/*!======= " + filename + " =======*/\n" + content, function () {
+        fs.appendFile('./easycss-1.2.0.css', "\n/*!======= " + filename + " =======*/\n" + content, function () {
             console.log('\x1B[37m%s\x1B[39m', new Date() + ':' + filename + '合并成功\n');
         });
     });
